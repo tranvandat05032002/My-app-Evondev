a1094ff8912840ca30d24f0ae85d2edf11ce70aa
Spider Man <tranvandathtml0503@gmail.com>
2022-10-23 22:35:50 +0700

    Prop getter

--- src/component/advanced/propCollection/SwitchRender.js
+++ src/component/advanced/propCollection/SwitchRender.js
@@ -6,23 +6,38 @@ function useToggle() {
   const [on, setOn] = useState(false);
 
   const toggle = () => setOn(!on);
+  function getToggleProps({ onClick, ...rest }) {
+    return {
+      onClick: () => {
+        onClick && onClick();
+        toggle();
+      },
+      ...rest,
+    };
+  }
   return {
     on,
     toggle,
-    toggleProps: {
-      onClick: toggle,
-    },
+    getToggleProps,
+    // toggleProps: {
+    //   onClick: toggle,
+    // },
+    //Props getter
   };
 }
 
 const SwitchRender = () => {
-  const { on, toggleProps } = useToggle();
+  const { on, getToggleProps } = useToggle();
   return (
     <div>
       {/* <Switch on={on} onClick={toggleProps.onClick}></Switch> */}
-      <Switch on={on} {...toggleProps}></Switch>
+      {/* <Switch on={on} {...toggleProps}></Switch> */}
+      <Switch {...getToggleProps({ on })}></Switch>
       <hr />
-      <button aria-label="custom-button" {...toggleProps}>
+      <button
+        aria-label="custom-button"
+        {...getToggleProps({ onClick: () => console.log("onButtonClicked") })}
+      >
         {on ? "On" : "Off"}
       </button>
     </div>
