4f47726669b5969409796960ceabb2dc79bdabc7
Spider Man <tranvandathtml0503@gmail.com>
2022-10-27 02:31:13 +0700

    Full performane re-render component

--- src/component/advancedPattern/performance/RenderCounter.js
+++ src/component/advancedPattern/performance/RenderCounter.js
@@ -6,13 +6,13 @@ import Counter from "./Counter";
 // React.memo(Component)
 // -> Component sẽ bị re-render khi state hoặc event(onChange) của nó thay đổi(trong trường hợp này là count)
 // -> Component sẽ bị re-render khi nó có props truyền vào thay đổi
+//Memorized
 const RenderCounter = () => {
   const [filter, setFilter] = useState("");
-  //   const calculator = () => {
-  //     setFilter("");
-  //   };
-  //   const calculator = useCallback(() => setFilter(""), [setFilter]);
-  const calculator = useMemo(() => setFilter(""), [setFilter]);
+  //useCallback
+  const calculator = useCallback(() => setFilter(""), [setFilter]);
+  //useMemo
+  const data = useMemo(() => ({ success: false }), []);
 
   return (
     <div className="p-4">
@@ -23,7 +23,7 @@ const RenderCounter = () => {
         value={filter}
         onChange={(e) => setFilter(e.target.value)}
       />
-      <Counter calculator={calculator}></Counter>
+      <Counter calculator={calculator} data={data}></Counter>
     </div>
   );
 };
