09219ec3ea133d3e772339a26927b23c21434fbd
Spider Man <tranvandathtml0503@gmail.com>
2022-10-27 12:51:38 +0700

    Get data from fireBase

--- src/fireBase/FirebaseApp.js
+++ src/fireBase/FirebaseApp.js
@@ -1,6 +1,20 @@
-import React from "react";
-
+import React, { useEffect } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "./firebase-config";
 const FirebaseApp = () => {
+  useEffect(() => {
+    const colRef = collection(db, "posts");
+    getDocs(colRef).then((snapshot) => {
+      let post = [];
+      snapshot.docs.forEach((doc) => {
+        post.push({
+          id: doc.id,
+          ...doc.data(),
+        });
+      });
+      console.log(post);
+    });
+  });
   return <div></div>;
 };
 
